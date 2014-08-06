6a4cd9a2e5817d5714210ac8fcf3f80bbfc30255
myersb <Ben@myhotspot.cmhcorp.local>
2014-08-06 10:15:36 -0400

    Initial Commit

---
+++ SlideOutMenu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlideOutMenu
+//
+//  Created by Benjamin Myers on 8/6/14.
+//  Copyright (c) 2014 appguys.biz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlideOutMenu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SlideOutMenu
+//
+//  Created by Benjamin Myers on 8/6/14.
+//  Copyright (c) 2014 appguys.biz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
